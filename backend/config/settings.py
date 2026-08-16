6fc6342218de38ee3c554dfbf69ff7fc4c93695c
VelunaiTech <velunai02724@gmail.com>
2026-08-17 00:12:50 +0530

    fixes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -111,15 +111,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import dj_database_url
+#import dj_database_url
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+    }
+#   "default": dj_database_url.config(
+#        default=os.environ.get("DATABASE_URL"),
+#        conn_max_age=600,
+#        ssl_require=True,
+#    )
 }
+
+
 
 
 # Password validation
