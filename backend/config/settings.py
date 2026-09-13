7becd05d6bb2fa1929c9157ef49169a5a53f8acc
VelunaiTech <velunai02724@gmail.com>
2026-09-13 16:09:46 +0530

    render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,13 +25,9 @@ except ImportError:
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n419nmu+oj)rif&5#q6@u6$i3-!_3g1+bv1%-47qr%i%h475^='
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-for-local-dev')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'api.velunai.in').split(',')
 
 
 # Application definition
